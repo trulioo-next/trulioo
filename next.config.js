959f50b6758ce46c3f5253a5dae821806434687a
shutter111 <rob@poundandgrain.com>
2020-03-05 21:21:11 -0500

    deploy to now

--- next.config.js
+++ next.config.js
@@ -43,11 +43,11 @@ const nextConfig = {
   },
   
   // BEFORE DEPLOY, ADD THESE 
-  // ROOT_URL:"https://7-11.now.sh",
+  // ROOT_URL:"https://7-11.rob24.now.sh",
   // ENDPOINT_URL:"https://dev3.7eleven.ca/wp-json"
 
   env: {
-    ROOT_URL:"https://7-11.now.sh",
+    ROOT_URL:"https://7-11.rob24.now.sh",
     ENDPOINT_URL:"https://dev3.7eleven.ca/wp-json"
   }
 };
