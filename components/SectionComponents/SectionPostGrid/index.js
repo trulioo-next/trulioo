702b340083a3f0ff0c10f92df5d614f7d7b9be64
shutter111 <rob@poundandgrain.com>
2020-03-25 23:33:24 -0400

    category page data ready

---
+++ components/SectionComponents/SectionPostGrid/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+import ColumnSpread from '@/components/ColumnSpread';
+import MediaObjectCard from '@/components/MediaObjectCard';
+ 
+
+const SectionPostGrid = (props) => {
+
+	let { posts, category } = props
+	let count = posts.length;
+ 
+    return <ColumnSpread spread={count}>
+            
+	{ posts &&
+		posts.map((post, sectionKey) => (
+		<MediaObjectCard key={sectionKey}
+		title={post.title}
+		image="/static/images/placeholders/NewYorkDeliMeatLovers.png"
+		product={{ href: '/menu/'+category+'/'+post.slug, calories: post.calories }}
+		stacked
+		>
+		{post.description}
+		</MediaObjectCard>
+		))
+	}
+
+
+          </ColumnSpread>
+};
+
+SectionPostGrid.defaultProps = {};
+
+export default SectionPostGrid;
