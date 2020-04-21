997dfe1075242517794320f869dcd49c2b6ee1be
shutter111 <rob@poundandgrain.com>
2020-04-21 11:06:14 -0400

    full auth flow 2

--- stores/user/sagas.js
+++ stores/user/sagas.js
@@ -49,7 +49,7 @@ function* facebookUserAuth(payload) {
    
     const loginClientResponse = yield call(sevenRewardsService.userAuthFb, payload)
 
-    console.log('FACEBOOK LOGIN SAGA !!   ', loginClientResponse )
+    console.log('FACEBOOK LOGIN SAGA !!   ', loginClientResponse, payload  )
 
     yield put({ type:SEVEN_REWARDS_FACEBOOK_AUTH, payload:loginClientResponse})
 
