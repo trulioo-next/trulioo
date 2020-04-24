93278efab386d2181bcd8afef7f6ea47601493a2
shutter111 <rob@poundandgrain.com>
2020-04-24 13:00:31 -0400

    updated facebook authentication

--- pages/api/seven-rewards/user-auth-fb.js
+++ pages/api/seven-rewards/user-auth-fb.js
@@ -2,6 +2,11 @@ const fetch = require('../../../utils/fetch')
 const REWARDS_API_URL = "https://api.7-eleven.com"
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
+const CLIENT_ID_ANNO = process.env.CLIENT_ID_ANNO
+const CLIENT_SECRET_ANNO = process.env.CLIENT_SECRET_ANNO
+
+
+import Rewards from './user-rewards';
  
 export default async (req, res) => {  
   try {
@@ -9,8 +14,6 @@ export default async (req, res) => {
      
     const body = JSON.parse(req.body)
     const headers = { "Content-Type": "application/json" };
-
-    console.log('FACEBOOK LOGIN CALL ')
 
      
     // Get an Access Token
@@ -20,26 +23,31 @@ export default async (req, res) => {
        method: 'POST',
        headers: headers,
        body: JSON.stringify({
-       "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "provider": "facebook", 
-        'access_token':req.body
+       "client_id": CLIENT_ID_ANNO,
+       "client_secret": CLIENT_SECRET_ANNO,
+       "provider": "facebook", 
+       'access_token':req.body
       }) 
      });
       
-    // let user = {
-    //   userName:body.userName,
-    //   isAuth: true,
-    //   token: userToken.access_token,
-    //   expire:userToken.expires_in
-    // }
 
-    console.log('FULL USER DATA   :: ', userToken )   
+   // Get a rewards list 
+    const userRewards = Rewards();
+    let rewards = await userRewards.getUserRewards(userToken.access_token, userToken.expires_in)
+    //
+    // console.log('REGITERS TOKEN ', rewards )
+      
+    let userAuthToken = {
+      isAuth: true,
+      token: userToken.access_token,
+      expire:userToken.expires_in
+    }
+
+    res.json({user:rewards.fullProfile, rewards:rewards.rewards, auth:userAuthToken, coupons:rewards.coupons, deals:rewards.deals, promotions:rewards.promotions, error:false  })  
  
-     res.json(userToken)
      return
   } catch(error) {
-    res.json({error: error })
+    res.json({error: error, user:false, rewards:false, auth:false, coupons:false, deals:false, promotions:false  })
     // res.status(400).send({ error: error })
   }
 };
