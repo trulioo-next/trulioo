9ed65fc449832b89838b64bce93341ae36cfa0c3
Leo Nanfara <leo@pulsarmedia.ca>
2020-04-27 21:21:25 -0400

    7rewards tabs data are now being populated

--- components/7rewards/blocks/items/ListItemOffer.js
+++ components/7rewards/blocks/items/ListItemOffer.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import './ListItemOffer.scss'
 
-const ListItemOffer = () => {
+const ListItemOffer = ({ data }) => {
+    console.log(data);
     return (
-        <div className="list-item offer-item"
-            data-offer-id="1"
-            data-offer-name="title"
-            data-offer-expiry="1"
-            data-toggle="tab" data-target="#offer_1"
-        >
-            <div className="item-card">
-                <div className="item-image">
-                    <img src="https://place-hold.it/130x130/#ccc" />
-                </div>
-                <div className="item-content">
-                    <span className="item-value">+600 Points</span>
-                    <span className="item-description">When you buy any 2 Old Dutch® XXL/ Kettle Potato Chips 180-255g, Ridgies & Dutch Crunch 180-255g</span>
-                    <span className="item-expiry">Expires 07/01/20</span>
-                </div>
+        <>
+            {data && <div className="list-item offer-item"
+                data-offer-id="1"
+                data-offer-name="title"
+                data-offer-expiry="1"
+                data-toggle="tab" data-target="#offer_1"
+            >
+                <div className="item-card">
+                    <div className="item-image">
+                        <img src={data.image_thumb} />
+                    </div>
+                    <div className="item-content">
+                        <span className="item-value" dangerouslySetInnerHTML={{ __html: data.title }} />
+                        <span className="item-description" dangerouslySetInnerHTML={{ __html: data.description }} />
+                        <span className="item-expiry" dangerouslySetInnerHTML={{ __html: data.expiration_label }} />
+                    </div>
 
-            </div>
-        </div>
+                </div>
+            </div>}
+
+        </>
+
     )
 }
 
