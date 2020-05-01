28414f8dd5ba601651e0f95d818024c21c4e51a6
shutter111 <rob@poundandgrain.com>
2020-05-01 10:37:08 -0400

    SEVENELEVENCA-67

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -4,22 +4,12 @@ import './NavBar.scss';
 import Branding from './components/Branding';
 import DesktopNav from './components/DesktopNav';
 import OffCanvasNav from './components/OffCanvasNav';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-
-
+ 
 const NavBar = data => (
   <nav className="SiteHeader">
-   
     <Branding />
     <OffCanvasNav data={data} />
     <DesktopNav data={data}  />
-     
-  
-  
   </nav>
 );
 export default NavBar;
--- components/ProductCard/ProductCard.js
+++ components/ProductCard/ProductCard.js
@@ -18,23 +18,27 @@ const ProductCard = ({ item }) => {
 
   return (
     <Card className={cardClasses}>
-      <Card.Body className={bodyClasses}>
-        <div className="Card__image -contain -square">
-          <img src={image} />
-        </div>
-        <h3 className={classNames('h4', 'Card__title')}>{props.title}</h3>
-        <div className="Card__content">
-          <p>{props.excerpt}</p>
-        </div>
-      </Card.Body>
       <Link href={props.href}>
         <a className="Card__linkedFooter">
-          <Card.Footer className="Card__footer d-flex justify-content-between">
-            <span>View product details</span>
-            <span>{props.calories} cal</span>
-          </Card.Footer>
+        <Card.Body className={bodyClasses}>
+          <div className="Card__image -contain -square">
+          
+              <img src={image} />
+            
+          </div>
+          <h3 className={classNames('h4', 'Card__title')}>{props.title}</h3>
+          <div className="Card__content">
+            <p>{props.excerpt}</p>
+          </div>
+        </Card.Body>
+        
+            <Card.Footer className="Card__footer d-flex justify-content-between">
+              <span>View product details</span>
+              <span>{props.calories} cal</span>
+            </Card.Footer>
+          
         </a>
-      </Link>
+        </Link>
     </Card>
   );
 };
