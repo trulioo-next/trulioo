586744c4cad22c4f9b409d27fbbc036fbeb2930f
Anderson Nguyen <anderson@idearebel.com>
2020-05-04 15:25:24 -0700

    Style Sign Out page

--- pages/7rewards/signout.js
+++ pages/7rewards/signout.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Layout from '../../containers/Layout/Layout';
 import Header from '../../components/Header/Header';
-import Button from '@/components/Button';
 import appActions from '../../stores/user/actions';
 import appSelectors from '../../stores/user/selectors';
-import Hero from '@/components/Hero';
-import { css, jsx } from '@emotion/core';
-import Link from 'next/link';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import AdminPanel from '@/components/7rewards/Admin/AdminPanel';
+
+import SevenRewardsLogo from '@/static/images/7rewards/7-rewards-logo.svg';
 
 class UserAuth extends React.Component {
   constructor(props) {
@@ -35,24 +39,29 @@ class UserAuth extends React.Component {
   //
   componentDidUpdate() {
     let isUserAuth = this.props.user.auth;
-    console.log('THIS USER ', isUserAuth);
+    // console.log('THIS USER ', isUserAuth);
   }
 
   render() {
     return (
       <Layout>
         <Header title="Sign Out" />
-        <Hero src="/static/images/placeholders/Get7Rewards_Background.jpg"></Hero>
-        <div className="login__screen__page">
-          <div
-            className="form__wrapper"
-            css={css`
-              margin-top: 150px;
-            `}
-          >
-            <h2>You have successfully logged out</h2>
-          </div>
-        </div>
+        <Container className="my-5 px-5 px-md-4">
+          <Row className="justify-content-center py-lg-4 my-5">
+            <Col xs="6" md="3">
+              <SevenRewardsLogo />
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col xs="12" md="10" lg="8">
+              <AdminPanel className="mt-n5 p-5">
+                <h1 className="h4 text-center m-0">
+                  You have successfully logged out.
+                </h1>
+              </AdminPanel>
+            </Col>
+          </Row>
+        </Container>
       </Layout>
     );
   }
