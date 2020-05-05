c2e1608b8fd8c87d3e73931e1fe4d660b9ecdc75
shutter111 <rob@poundandgrain.com>
2020-05-05 15:30:39 -0400

    mobile overflow test

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import SectionMaker from '../components/SectionMaker';
 import Layout from '../containers/Layout/Layout';
 import Header from '../components/Header/Header';
+import { NextSeo } from 'next-seo';
 
 import Error from 'next/error';
 
@@ -21,11 +22,19 @@ const Home = props => {
 
   const pageData = useSelector(state => pageDataSelector(state));
   let data = pageData && pageData.acf_data && pageData.acf_data.components ? pageData.acf_data : false;
- 
+  
 
+  let seoTitle = data && pageData && pageData.seo && pageData.seo.title !== '' ? pageData.seo.title : 'Home - 7-11 Canada'
+  let seoDesc = data && pageData && pageData.seo ? pageData.seo.desc : ''
+
+  console.log('seoTitle ::>> ', seoTitle )
   return (
     <Layout>
-      <Header title="" />
+    <NextSeo
+      title={seoTitle}
+      description={seoDesc}
+    />
+      <Header title={seoTitle} />
       { data &&
         data.components.map((section, sectionKey) => (
           <SectionMaker
