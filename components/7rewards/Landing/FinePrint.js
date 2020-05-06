79a2069f8293d5fda606b30926726d842e63c665
Russell Montgomery <russell@rustyramone.com>
2020-05-06 18:39:00 -0400

    [#116] 7Rewards fine print text updated

--- components/7rewards/Landing/FinePrint.js
+++ components/7rewards/Landing/FinePrint.js
@@ -11,9 +11,9 @@ const FinePrint = () => (
         <Col xs="12" md="10" lg="9" className="col">
           <Row className="text-center my-md-1 fineprint">
             <Col>
-			        Program available only to members of the 7Rewards® Program and available only at participating 7-Eleven® stores,
-              excluding Hawaii. Go to the 7-Eleven Mobile App for additional terms and conditions of 7Rewards. Points exclude
-              services, fuel, and age-restricted items including tobacco, lottery, and alcohol.
+			        Program available only to members of the 7Rewards® Program and available only at participating 7-Eleven® stores. 
+              Go to the 7-Eleven Mobile App for additional terms and conditions of 7Rewards. Points exclude
+              services, fuel, and age-restricted items including tobacco and lottery.
             </Col>
           </Row>
         </Col>
