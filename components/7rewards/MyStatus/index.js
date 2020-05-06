6d5326d9a3d87c6183d7516bdf754b8d6cc3d9e7
Anderson Nguyen <anderson@idearebel.com>
2020-05-06 11:04:47 -0700

    Add user barcode on mobile for 7Rewards.
    * Added: `bwip-js` for handling barcodes
    * Added: `RewardsBarcode` component to `MyStatus`
    * Change: adjust `MyStatus` styling

--- components/7rewards/MyStatus/index.js
+++ components/7rewards/MyStatus/index.js
@@ -8,6 +8,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import RewardsGraph from './RewardsGraph';
+import RewardsBarcode from './RewardsBarcode';
 
 const MyStatus = ({ data }) => {
   const [activePunchCard, setActivePunchCard] = useState(0);
@@ -51,13 +52,13 @@ const MyStatus = ({ data }) => {
 
   return (
     <aside className="MyStatus">
-      <Row className="align-items-center mx-md-n4">
-        <Col xs="12" md="6" lg="12" className="px-md-4">
+      <Row className="align-items-center">
+        <Col xs="12" md="6" lg="12" className="col">
           <div className="RewardGraph MyStatus__rewardGraph">
             <RewardsGraph tiers={tiers} points={data.rewards_points || 0} />
           </div>
         </Col>
-        <Col xs="12" md="6" lg="12" className="px-md-4 px-lg-0">
+        <Col xs="12" md="6" lg="12" className="col px-lg-0">
           <div className="PunchCard MyStatus__punchCard">
             <span
               className="PunchCard__heading text-center"
@@ -81,6 +82,12 @@ const MyStatus = ({ data }) => {
               Finish a punch card and earn a free item! Rules and products vary
               per punch card.
             </p>
+          </div>
+        </Col>
+        <Col xs="12" className="col d-md-none text-center">
+          <p>Scan this barcode in the store to receive and redeem points.</p>
+          <div className="px-4 mt-4">
+            <RewardsBarcode data={data.barcode} />
           </div>
         </Col>
       </Row>
