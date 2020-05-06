b28380701a3064c1d368b733bacd267aa63200dc
shutter111 <rob@poundandgrain.com>
2020-05-06 14:18:07 -0400

    facebook O-auth test 3

--- stores/user/sagas.js
+++ stores/user/sagas.js
@@ -64,8 +64,8 @@ function* facebookUserAuth(payload) {
    
     const loginClientResponse = yield call(sevenRewardsService.userAuthFb, payload)
 
-    // console.log('FACEBOOK LOGIN SAGA !!   ', loginClientResponse  )
-    // console.log('FACEBOOK payload !!   ', payload  )
+    console.log('FACEBOOK LOGIN SAGA !!   ', loginClientResponse  )
+    console.log('FACEBOOK payload !!   ', payload  )
 
     yield put({ type:SEVEN_REWARDS_FACEBOOK_AUTH, payload:loginClientResponse})
 
