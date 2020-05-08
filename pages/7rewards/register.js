1ad1e3ba3de0f50aea311290f5aa5c9304046e05
Robbie Barnett-Kemper <robbie@idearebel.com>
2020-05-08 18:57:09 -0400

    Adding recaptcha hiding and language

--- pages/7rewards/register.js
+++ pages/7rewards/register.js
@@ -668,6 +668,7 @@ class RegisterScreen extends React.Component {
                                   </Form.Check>
                                 </Form.Group>
                               </fieldset>
+                              <p class='recaptcha-language'>This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.</p>
                               <Button type="submit" className="mt-4" onClick={(e) => this.submitForm(e)}>
                                 Register
                               </Button>
