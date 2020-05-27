7812599560fa9ba5c7f7422fadce797dabd64812
Anderson Nguyen <anderson@idearebel.com>
2020-05-27 12:51:55 -0700

    Fix browser resizing errors on parallax components.
    `setWindowWidth` was undefined due to the hooks being renamed to `setWindowInnerWidth`
    
    * Change: update window width hooks

--- components/Parallax/parallax-content.js
+++ components/Parallax/parallax-content.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect  } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
   motion,
@@ -7,36 +7,41 @@ import {
   useSpring,
 } from 'framer-motion';
 
-export const ParallaxContent = ({ children, topOffset = 83, bottomOffset = 83, range = 60 }) => {
-  
- const [ isWindow, setIsWindow ] = useState(false);
- const [ windowInnerWidth, setWindowInnerWidth] = useState(0);
- const [ windowInnerHeight, setWindowInnerHeight] = useState(0);
- const [ bgLoaded, setBgLoaded] = useState(false);
-  
-  if (process.browser && !bgLoaded ) {
-    setIsWindow(true)
+export const ParallaxContent = ({
+  children,
+  topOffset = 83,
+  bottomOffset = 83,
+  range = 60,
+}) => {
+  const [isWindow, setIsWindow] = useState(false);
+  const [windowInnerWidth, setWindowInnerWidth] = useState(0);
+  const [windowInnerHeight, setWindowInnerHeight] = useState(0);
+  const [bgLoaded, setBgLoaded] = useState(false);
+
+  if (process.browser && !bgLoaded) {
+    setIsWindow(true);
     setWindowInnerWidth(window.innerWidth);
     setWindowInnerHeight(window.innerHeight);
-    setBgLoaded(true)
+    setBgLoaded(true);
   }
 
-    
   const { scrollY } = useViewportScroll();
   const ref = useRef();
-  const [ minHeight, setMinHeight ] = useState('auto');
-  const [ triggerRange, setTriggerRange ] = useState({ start: 0, end: 0 });
-   
+  const [minHeight, setMinHeight] = useState('auto');
+  const [triggerRange, setTriggerRange] = useState({ start: 0, end: 0 });
+
   const breakpoint = 992;
 
   useEffect(() => {
     if (process.browser) {
       const handleResize = () => {
-        setWindowWidth(window.innerWidth);
+        setWindowInnerWidth(window.innerWidth);
       };
-    
+
       window.addEventListener('resize', handleResize);
-      return () => { window.removeEventListener('resize', handleResize); };
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
     }
   });
 
@@ -48,7 +53,7 @@ export const ParallaxContent = ({ children, topOffset = 83, bottomOffset = 83, r
         setMinHeight(ref.current.offsetHeight + range);
         setTriggerRange({
           start: ref.current.getBoundingClientRect().top - topOffset,
-          end: ref.current.getBoundingClientRect().top + bottomOffset
+          end: ref.current.getBoundingClientRect().top + bottomOffset,
         });
       }
     };
@@ -60,31 +65,36 @@ export const ParallaxContent = ({ children, topOffset = 83, bottomOffset = 83, r
         window.removeEventListener('resize', onResize);
       };
     }
-  }, [ ref, range, topOffset, bottomOffset, triggerRange.start, triggerRange.end ]);
+  }, [
+    ref,
+    range,
+    topOffset,
+    bottomOffset,
+    triggerRange.start,
+    triggerRange.end,
+  ]);
 
   const bounceY = useSpring(
-    useTransform(
-      scrollY,
-      [ triggerRange.start, triggerRange.end ],
-      [ 0, range ]
-    ),
+    useTransform(scrollY, [triggerRange.start, triggerRange.end], [0, range]),
     {
       damping: 100,
       stiffness: 100,
       mass: Math.floor(Math.random() * 3),
-    }
+    },
   );
 
   return windowInnerWidth > breakpoint ? (
-    <div className="parallax-content" style={ { minHeight } }>
-      <motion.div ref={ ref } initial={ { translateY: 0 } } style={ { translateY: bounceY } }>
+    <div className="parallax-content" style={{ minHeight }}>
+      <motion.div
+        ref={ref}
+        initial={{ translateY: 0 }}
+        style={{ translateY: bounceY }}
+      >
         {children}
       </motion.div>
     </div>
   ) : (
-    <div className="section-content">
-      {children}
-    </div>
+    <div className="section-content">{children}</div>
   );
 };
 
